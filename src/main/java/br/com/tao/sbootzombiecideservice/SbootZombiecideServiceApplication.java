37d1f09fa0431eae606d37cd0c9292de7ea34f2c
thiagooliveira-jusfy <thiago.oliveira@jusfy.com.br>
2026-03-01 19:43:19 -0300

    Create Project

---
+++ src/main/java/br/com/tao/sbootzombiecideservice/SbootZombiecideServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.tao.sbootzombiecideservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootZombiecideServiceApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(SbootZombiecideServiceApplication.class, args);
+      }
+
+}
---
+++ src/test/java/br/com/tao/sbootzombiecideservice/SbootZombiecideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tao.sbootzombiecideservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootZombiecideServiceApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
